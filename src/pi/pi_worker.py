4b7bd9cc190f54e1a225abbf0dc502999afb75a3
Rune Mæstad <54098894+rune-m@users.noreply.github.com>
2024-03-31 20:45:44 +0200

    Pi integration (#2)
    * Init
    
    * Temp: Disable Tono send report.
    
    * Temp: Enable Tono save report.
    
    * Import fix
    
    * Import fix
    
    * Import
    
    * Import
    
    * Test worker
    
    * Test worker
    
    * Pi worker good

---
+++ src/pi/pi_worker.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+sys.path.append('/'.join(__file__.split('/')[:-2]))
+import schedule
+import time
+from datetime import datetime
+from main_pi import run
+    
+schedule.every().monday.at("03:00").do(run)
+
+print(str(datetime.now()) + " - Starter TonoBot...")
+
+while 1:
+    try:
+        schedule.run_pending()
+        time.sleep(1)
+    except:
+        print(str(datetime.now()) + " - Error occured!")
