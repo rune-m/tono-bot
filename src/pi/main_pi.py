faada384d04fb2c49d0d424d38edb1eddb366484
Rune Mæstad <rune.maestad@gmail.com>
2024-03-31 19:48:38 +0200

    Import fix

--- src/pi/main_pi.py
+++ src/pi/main_pi.py
@@ -1,15 +1,12 @@
-from bot import run_bot
+from tono.tonobot import run_bot
 from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-def run():
-  display = Display(visible=0, size=(1600, 1600))
-  display.start()
+display = Display(visible=0, size=(1600, 1600))
+display.start()
 
-  service = Service(f'/usr/lib/chromium-browser/chromedriver')
-  driverLoader = lambda: webdriver.Chrome(service=service)
+service = Service(f'/usr/lib/chromium-browser/chromedriver')
+driverLoader = lambda: webdriver.Chrome(service=service)
 
-  run_bot(driverLoader)
-
-run()
+run_bot(driverLoader)
